4db6dc4c276d23d15995baf5f6d88a83f7c758d2
zarar384 <fel98_98@mail.ru>
2024-06-09 16:31:56 +0200

    implement ICarterModule and reguster MediatR, Carter libraries into Dependency Injection Service

--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,9 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    //automatically find and register all handlers for commands, queries from the current assembly
+    //where the Program class is located. 
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
