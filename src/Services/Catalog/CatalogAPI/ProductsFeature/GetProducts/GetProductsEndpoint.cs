06c212a5cd84eaa856e114e5d761d30db522f2ee
zarar384 <fel98_98@mail.ru>
2024-06-13 23:45:15 +0200

    added GetProducts and added docker-ocmpose for postresql image

---
+++ src/Services/Catalog/CatalogAPI/ProductsFeature/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using CatalogAPI.ProductsFeature.CreateProduct;
+
+namespace CatalogAPI.ProductsFeature.GetProducts
+{
+    //public record GetProductRequest();
+    public record GetProductResponse(IEnumerable<Product> Products);
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async ( ISender sender) =>
+            {
+                //var comand = reqest.Adapt<GetProductsQuery>();
+
+                var result = await sender.Send(new GetProductsQuery());
+
+                var response = result.Adapt<GetProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+        }
+    }
+}
---
+++ src/Services/Catalog/CatalogAPI/ProductsFeature/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace CatalogAPI.ProductsFeature.GetProducts
+{
+    public record GetProductsQuery():IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+    internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle with {@Guery}", query);
+
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
+    }
+}
