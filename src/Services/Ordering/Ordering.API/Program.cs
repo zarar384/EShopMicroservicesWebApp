ebf86d0a94c322f08ad8a2211b2c159b3d70f3c5
zarar384 <fel98_98@mail.ru>
2024-09-10 23:05:09 +0200

    Services\Ordering prepare clean architecture layer: Domain, Application, Infrastructure, API

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,24 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+/*add services to the container
+
+Infrastructure - EF Core
+Application - MediatR
+API - Carter, HealthChecks, ...
+*/
+
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
+var app = builder.Build();
+
+//configure the HTTP request pipeline
+
+app.Run();
---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectiongString = configuration.GetConnectionString("DefaultConnection");
+
+            //add services to the container.
+            //services. AddDbContext<ApplicationDbContext>(options =>
+            //options.UseSqlServer(connectionString));
+
+            //services. AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
