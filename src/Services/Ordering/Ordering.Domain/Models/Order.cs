6b750a26c3cfa3b328f0a7c28357c450fa791dc8
zarar384 <fel98_98@mail.ru>
2024-09-17 22:04:05 +0200

    prepared Ordering.Domain layer with Tactical DDD: Entities, ValueObjects(rich-domain model),Aggregates. Added strongly typed IDs for Order Entities

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Order: Aggregate<OrderId>
+    {
+        private readonly List<OrderItem> _orderItems = new();
+        public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+        public CustomerId CustomerId { get; private set; } = default!;
+        public OrderName OrderName { get; private set; } = default!;
+        public Address ShippingAddress { get; private set; } = default!;
+        public Address BillingAddress { get; private set; } = default!;
+        public Payment Payment { get; private set; } = default!;
+        public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+        public decimal TotalPrice
+        {
+            get => OrderItems.Sum(x => x.Price * x.Quantity);
+            private set { }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,37 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record Payment
+    {
+        public string? CardName { get; } = default!;
+        public string CardNumber { get; } = default!;
+        public string Expiration { get; } = default!;
+        public string CVV { get; } = default!;
+        public int PaymentMethod { get; } = default!;
+        public string CardName1 { get; }
+        public string CardNumber1 { get; }
+        public string Expiration1 { get; }
+        public string Cvv { get; }
+        public string PaymentMethod1 { get; }
+
+        protected Payment() { }
+
+        public Payment(string cardName, string cardNumber, string expiration, string cvv, string paymentMethod)
+        {
+            CardName1 = cardName;
+            CardNumber1 = cardNumber;
+            Expiration1 = expiration;
+            Cvv = cvv;
+            PaymentMethod1 = paymentMethod;
+        }
+
+        public static Payment Of(string cardName, string cardNumber, string expiration, string cvv, string paymentMethod)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
+
+            return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
+        }
+    }
+}
