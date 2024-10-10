590ad28acc5814d5dd2bb40386d8ad096a6cbe2e
zarar384 <fel98_98@mail.ru>
2024-10-10 22:58:32 +0200

    set API gateway and reverse proxy configuration.

--- src/ApiGateways/YarpApiGateways/Program.cs
+++ src/ApiGateways/YarpApiGateways/Program.cs
@@ -1,7 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
 
 //configure the HTTP request pipeline
+app.MapReverseProxy();
+
 app.Run();
