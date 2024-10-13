132ea6eaf7ce9529930601f4dad6e1bc49c9753b
zarar384 <fel98_98@mail.ru>
2024-10-13 18:12:58 +0200

    upd yarp seetings and containerize and orchestrate YarpApiGateway

--- src/Services/Basket/BasketAPI/BasketFeature/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/BasketAPI/BasketFeature/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,4 @@
-﻿using BasketAPI.Data;
-
-namespace BasketAPI.BasketFeature.DeleteBasket
+﻿namespace BasketAPI.BasketFeature.DeleteBasket
 {
     public record DeleteBasketCommand(string UserName): ICommand<DeleteBasketResult>;
     public record DeleteBasketResult(bool IsSuccess);
